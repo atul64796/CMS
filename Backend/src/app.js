4fb2200370b35e98f822f8ff09c34cf39f5bf916
atul64796 <atul64796@gmail.com>
2026-08-13 18:42:10 +0530

    fixed cors issue1

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,10 @@ import "./services/cron/reminderCron.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: 'https://cms-silk-gamma.vercel.app' }));
+app.use(cors({
+  origin: 'https://cms-silk-gamma.vercel.app', // Your specific frontend domain
+  credentials: true // Required to allow cookies/auth headers
+}));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
