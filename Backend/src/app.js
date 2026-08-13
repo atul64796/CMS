a0a724e762299587ceb5d7ed1919ef8a2305a8f7
atul64796 <atul64796@gmail.com>
2026-08-13 17:34:55 +0530

    frontend deployed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,8 +11,13 @@ dotenv.config();
 const app = express();
 
 // ✅ middlewares
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://cms-silk-gamma.vercel.app",
   credentials: true
 }));
 
