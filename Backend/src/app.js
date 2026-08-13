f3ca7d3e9920d4ffd9c305ba4f14f6c99c2d8cb5
atul64796 <atul64796@gmail.com>
2026-08-13 18:16:49 +0530

    fix production cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -21,17 +21,12 @@ const allowedOrigins = [
   "https://cms-silk-gamma.vercel.app"
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
-
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
