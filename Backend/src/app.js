7acbec95554b94fa75b5faf91745b427f930240b
atul64796 <atul64796@gmail.com>
2026-08-13 18:09:47 +0530

    test-3

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,10 +16,22 @@ const app = express();
 //   credentials: true
 // }));
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://cms-silk-gamma.vercel.app"
+];
+
 app.use(cors({
-  origin: "https://cms-silk-gamma.vercel.app",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
